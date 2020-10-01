a6c11939fae523e6dbeee9de53c26d2f8e901f6b
Shruti <52789421+ShrutiSara@users.noreply.github.com>
2020-10-01 17:19:02 +0530

    homepage optforms

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Feature } from "../components";
+import { OptForm } from "../components";
 import { HeaderContainer } from "../containers/header";
 import { FooterContainer } from "../containers/footer";
 import { JumbotronContainer } from "../containers/jumbotron";
@@ -12,7 +13,12 @@ export default function Home() {
         <Feature>
           <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
           <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
-          {/* Copy paste opt form code here */}
+          <OptForm>
+            <OptForm.Input placeholder="Email Address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
+          </OptForm>
         </Feature>
       </HeaderContainer>
 
