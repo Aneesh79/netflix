b10e35cb415c63e2ff0001bac153782f5d3bc1a5
Shruti <52789421+ShrutiSara@users.noreply.github.com>
2020-10-05 18:23:09 +0530

    browse feature component

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -49,7 +49,7 @@ export const Link = styled.p`
   color: white;
   text-decoration: none;
   margin-right: 30px;
-  font-weight: ${({ active }) => (active === true ? "700" : "normal")};
+  font-weight: ${({ active }) => (active === "true" ? "700" : "normal")};
   cursor: pointer;
 
   &:hover {
